6c637ca09cc13aebc72763f5bb2b4a3c9e5b6196
Gino Bautista <bautista.gino03@gmail.com>
2020-01-06 20:19:06 +0800

    initial commit

---
+++ src/main/java/com/crescendia/crescendiaapp/CrescendiaAppApplication.java
@@ -0,0 +1,13 @@
+package com.crescendia.crescendiaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrescendiaAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrescendiaAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crescendia/crescendiaapp/CrescendiaAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crescendia.crescendiaapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrescendiaAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
